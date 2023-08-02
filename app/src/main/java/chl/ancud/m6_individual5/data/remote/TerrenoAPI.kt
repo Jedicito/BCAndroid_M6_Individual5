f2620b79aad9891bcd9ff4a97f4098dbaac7631d
Jedicito <seron.cesar@gmail.com>
2023-08-01 21:53:28 -0400

    Se agrega data class, interface y cliente retrofit

---
+++ app/src/main/java/chl/ancud/m6_individual5/data/remote/TerrenoAPI.kt
@@ -0,0 +1,10 @@
+package chl.ancud.m6_individual5.data.remote
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface TerrenoAPI {
+
+    @GET("realstate")
+    suspend fun getData(): Response<List<Terreno>>
+}
---
+++ app/src/main/java/chl/ancud/m6_individual5/data/remote/TerrenoRetrofit.kt
@@ -0,0 +1,20 @@
+package chl.ancud.m6_individual5.data.remote
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class TerrenoRetrofit {
+    companion object {
+        private const val URL_BASE =
+            "https://android-kotlin-fun-mars-server.appspot.com/"
+
+        fun getRetrofitTerreno() : TerrenoAPI {
+            val mRetrofit = Retrofit.Builder()
+                .baseUrl(URL_BASE)
+
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return mRetrofit.create(TerrenoAPI::class.java)
+        }
+    }
+}
